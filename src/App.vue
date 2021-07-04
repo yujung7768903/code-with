3fa99f7ae4d02ee89d0886736993e0e56cc7a1b8
yujung7768903 <yu-jung31476@naver.com>
2021-07-04 14:36:16 +0900

    프로젝트 시작

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header/>
+  <div class="guide">
+    <div class="greeting">
+      Welcome!
+      Free Course For Beginner - 
+      당신의 코딩학습에  코드윗이 함께할게요.
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/layout/Header-dark.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
+:root {
+    /* color */
+    --color_main-purple : #8944FA;
+    --color_light-purple : #8944FA;
+    --color_dark-purple : #4C1993;
+    --color_navy : #30355C;
+    --color_point-mint : #00D1FF;
+
+    --color_white : #FFFFFF;
+    --color_black : #000000;
+    --color_light-gray : #F5F5F5;
+    --color_gray : #D2D2D2;
+    --color_dark-gray : #898A8D;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+body {
+  background-image: url("./assets/background-main.png");
+  background-size: cover;
+}
+div {
+  box-sizing: border-box;
+}
+.guide .greeting {
+  color: var(--color_white);
+}
+
 </style>
---
+++ src/components/layout/Header-dark.vue
@@ -0,0 +1,68 @@
+<template>
+    <div id="header">        
+        <div id="logo">
+            <img id="logo-letter" src="../../assets/logo-letter.png" alt="">
+            <img id="logo-img" src="../../assets/main-page-logo.svg" alt="">
+        </div>
+        <div class="nav-menu">
+            <a href="" class="menu">Sign up</a>
+            <img class="nav-menu-division" src="../../assets/nav-menu-division.svg" alt="">
+            <a href="" class="menu">Login</a>
+            <img class="nav-menu-division" src="../../assets/nav-menu-division.svg" alt="">
+            <a href="" class="menu">My page</a>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: "header-dark",
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style>
+    body {
+        padding: 30px 40px;
+    }
+    div{
+        box-sizing: border-box;
+    }
+    a {
+        text-decoration: none;
+    }
+    #header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%; height: 77px;
+        background-color: rgba(254, 249, 249, 0.1);
+        border-radius: 20px;
+        padding: 0px 30px;
+        font-weight: 500;
+    }
+    #logo {
+        display: flex;
+        align-items: center;
+    }
+    #logo-letter {
+        width: 110px;
+    }
+    #logo-img {
+        width: 100px;
+        margin-left: 30px;
+    }
+
+    .nav-menu .menu {
+        color: #FFFFFF;
+        font-size: 18px;
+    }
+    .nav-menu .nav-menu-division {
+        margin: 0px 14px;
+    }
+</style>
